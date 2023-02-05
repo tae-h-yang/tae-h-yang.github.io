b0b656d648daa91fa913280096dd83668a163c2a
hooniii98 <taehoon.yang98@gmail.com>
2023-02-05 18:44:02 -0500

    post

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,5 +1,5 @@
 var store = [{
-        "title": "github.io 블로그 첫 포스팅입니다",
+        "title": "About",
         "excerpt":"여기엔 Markdown문법으로 적으면 된다. 나는 블로그를 만들었을때 시간이 많이 들었지만, 다른사람들은 이글을 보고 조금 더 빨리 만들었으면 좋겠다.  ","categories": [],
         "tags": ["Blog"],
         "url": "/githubpages/",

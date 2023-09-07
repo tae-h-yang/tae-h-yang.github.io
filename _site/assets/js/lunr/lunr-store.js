4a5ddee6d5e0c4e9b04cc03908c2c31f0b2210aa
tae-h-yang <taehoon.yang98@gmail.com>
2023-09-07 01:56:29 -0400

    Added course layout

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,4 +1,10 @@
 var store = [{
+        "title": "Garbage In, Garbage Out Revisited: Labeling and Dataification Practices Across Disciplines.",
+        "excerpt":"Abstract: “I discuss a range of issues and best practices around data labeling, verification, and quality across disciplines. This work is based on our existing research in the ‘Garbage In, Garbage Out’ project, as well as my experiences collaborating with researchers across disciplines.”  ","categories": [],
+        "tags": [],
+        "url": "/courses/tst/",
+        "teaser": null
+      },{
         "title": "About",
         "excerpt":"여기엔 Markdown문법으로 적으면 된다. 나는 블로그를 만들었을때 시간이 많이 들었지만, 다른사람들은 이글을 보고 조금 더 빨리 만들었으면 좋겠다.  ","categories": [],
         "tags": ["Blog"],

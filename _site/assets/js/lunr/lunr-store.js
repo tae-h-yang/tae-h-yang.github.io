07904237cce1a179b16647391e63a9907224862d
tae-h-yang <taehoon.yang98@gmail.com>
2023-09-08 00:35:20 -0400

    Extened width for some posts and pages

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -5,6 +5,12 @@ var store = [{
         "url": "/courses/eece5554/",
         "teaser": null
       },{
+        "title": "EECE5644 Introduction to Machine Learning and Patter Recognition",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/courses/eece5644/",
+        "teaser": null
+      },{
         "title": "About",
         "excerpt":"여기엔 Markdown문법으로 적으면 된다. 나는 블로그를 만들었을때 시간이 많이 들었지만, 다른사람들은 이글을 보고 조금 더 빨리 만들었으면 좋겠다.  ","categories": [],
         "tags": ["Blog"],

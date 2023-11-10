958cd1aaa42fa81cf3d08e5db28f05972d5d39b3
tae-h-yang <taehoon.yang98@gmail.com>
2023-11-10 00:04:37 -0500

    Added link disabling for courses page

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -56,85 +56,85 @@ var store = [{
         "title": "MATH2341 Differential Equations and Linear Algebra for Engineering",
         "excerpt":"Description: Studied ordinary differential equations, their applications, and techniques for solving them including numerical methods (using MATLAB), Laplace transforms, and linear algebra. Topics included linear and nonlinear first- and second-order equations and applications included electrical and mechanical systems, forced oscillation, and resonance. Topics from linear algebra, such as matrices, row-reduction,...","categories": [],
         "tags": [],
-        "url": "/courses/math2341/",
+        "url": "/courses/2019-1-7-MATH2341/",
         "teaser": null
       },{
         "title": "EECE2150 Circuits and Signals: Biomedical Applications",
         "excerpt":"Description: Covered circuit theory, signal processing, circuit building, and MATLAB programming. Introduced basic device and signal models and circuit laws used in the study of linear circuits. Analyzed resistive and complex impedance networks. Used the ideal operational amplifier model, focusing on differential amplifiers and active filter circuits. Introduced basic concepts...","categories": [],
         "tags": [],
-        "url": "/courses/eece2150/",
+        "url": "/courses/2019-9-4-EECE2150/",
         "teaser": null
       },{
         "title": "EECE2160 Embedded Design: Enabling Robotics",
         "excerpt":"Description: Covered the basics of the Unix operating system, high-level programming concepts, introductory digital design, wireless networking, and Simulink design. Offered a hands-on experience developing a remote-controlled robotic arm using an embedded systems platform.  ","categories": [],
         "tags": [],
-        "url": "/courses/eece2160/",
+        "url": "/courses/2019-9-4-EECE2160/",
         "teaser": null
       },{
         "title": "EECE2540 Fundamentals of Networks",
         "excerpt":"Description: Presented an overview of modern communication networks. The concept of a layered network architecture was used as a framework for understanding the principal functions and services required to achieve reliable end-to-end communications. Topics included service interfaces and peer-to-peer protocols, a comparison of the OSI (open system interconnection) reference model...","categories": [],
         "tags": [],
-        "url": "/courses/eece2540/",
+        "url": "/courses/2019-9-4-EECE2540/",
         "teaser": null
       },{
         "title": "EECE2412 Fundamentals of Electronics",
         "excerpt":"Description: Reviewed basic circuit analysis techniques. Briefly introduced operation of the principal semiconductor devices: diodes, field-effect transistors, and bipolar junction transistors. Covered diode circuits in detail; the coverage of transistor circuits focused mainly on large-signal analysis, DC biasing of amplifiers, and switching behavior. Used PSpice software to simulate circuits and...","categories": [],
         "tags": [],
-        "url": "/courses/eece2412/",
+        "url": "/courses/2020-1-6-EECE2412/",
         "teaser": null
       },{
         "title": "EECE2520 Fundamentals of Linear Systems",
         "excerpt":"Description: Developed the basic theory of continuous and discrete systems, emphasizing linear time-invariant systems. Discussed the representation of signals and systems in both the time and frequency domain. Topics included linearity, time invariance, causality, stability, convolution, system interconnection, and sinusoidal response. Developed the Fourier and Laplace transforms for the discussion...","categories": [],
         "tags": [],
-        "url": "/courses/eece2520/",
+        "url": "/courses/2020-1-6-EECE2520/",
         "teaser": null
       },{
         "title": "EECE2560 Fundamentals of Engineering Algorithms",
         "excerpt":"Description: Covered the design and implementation of algorithms to solve engineering problems using a high-level programming language. Reviewed elementary data structures, such as arrays, stacks, queues, and lists, and introduces more advanced structures, such as trees and graphs and the use of recursion. Covered both the algorithms to manipulate these...","categories": [],
         "tags": [],
-        "url": "/courses/eece2560/",
+        "url": "/courses/2020-1-6-EECE2560/",
         "teaser": null
       },{
         "title": "EECE2322 Fundamentals of Digital Design and Computer Organization",
         "excerpt":"Description: Covered the design and evaluation of control and data structures for digital systems. Used hardware description languages to describe and design both behavioral and register-transfer-level architectures and control units. Topics included number systems, data representation, a review of combinational and sequential digital logic, finite state machines, arithmetic-logic unit (ALU)...","categories": [],
         "tags": [],
-        "url": "/courses/eece2322/",
+        "url": "/courses/2022-9-7-EECE2322/",
         "teaser": null
       },{
         "title": "EECE5550 Mobile Robotics",
         "excerpt":"Description: Investigated the science and engineering of mobile robots. Topics included kinematics, dynamics, numerical methods, state estimation, control, perception, localization and mapping, and motion planning for mobile robots. Emphasizeed practical robot applications ranging from disaster response to healthcare to space exploration.  ","categories": [],
         "tags": [],
-        "url": "/courses/eece5550/",
+        "url": "/courses/2022-9-7-EECE5550/",
         "teaser": null
       },{
         "title": "MATH3081 Probability and Statistics",
         "excerpt":"Description: Focused on probability theory. Topics included sample space; conditional probability and independence; discrete and continuous probability distributions for one and for several random variables; expectation; variance; special distributions including binomial, Poisson, and normal distributions; law of large numbers; and central limit theorem. Also introduced basic statistical theory including estimation...","categories": [],
         "tags": [],
-        "url": "/courses/math3081/",
+        "url": "/courses/2022-9-7-MATH3081/",
         "teaser": null
       },{
         "title": "ME3460 Robot Dynamics and Control",
         "excerpt":"Description: Covered fundamental components and mechanisms of robotic systems and their multidisciplinary nature. Introduced the robot’s kinematics, dynamics, and control. Presented a quick overview of forward and inverse kinematics, robot dynamics, as well as path planning and control techniques. Topics also included dynamic modeling and analysis of mechanically, electrically, and...","categories": [],
         "tags": [],
-        "url": "/courses/me3460/",
+        "url": "/courses/2022-9-7-ME3460/",
         "teaser": null
       },{
         "title": "EECE4630 Robotics",
         "excerpt":"Description: Introduced robotics analysis covering basic theory of kinematics, dynamics, and control of robots. Developed design capabilities of microprocessor-based control systems with input from sensory devices and output actuators by designing and implementing a small mobile robot system to complete a specific task. Covered actuators, sensors, system modeling, analysis, and...","categories": [],
         "tags": [],
-        "url": "/courses/eece4630/",
+        "url": "/courses/2023-1-9-EECE4630/",
         "teaser": null
       },{
         "title": "EECE5554 Robotics Sensing and Navigation",
         "excerpt":"Description: Examined the actual sensors and mathematical techniques for robotic sensing and navigation with a focus on sensors such as cameras, sonars, and laser scanners. These were used in association with techniques and algorithms for dead reckoning and visual inertial odometry in conjunction with GPS and inertial measurement units. Covered...","categories": [],
         "tags": [],
-        "url": "/courses/eece5554/",
+        "url": "/courses/2023-1-9-EECE5554/",
         "teaser": null
       },{
         "title": "EECE5644 Introduction to Machine Learning and Patter Recognition",
         "excerpt":"Description: Studied machine learning (the study and design of algorithms that enable computers/machines to learn from experience/data). Covered a range of algorithms, focusing on the underlying models between each approach. Emphasized the foundations to prepare for research in machine learning. Topics included Bayes decision theory, maximum likelihood parameter estimation, model...","categories": [],
         "tags": [],
-        "url": "/courses/eece5644/",
+        "url": "/courses/2023-5-8-EECE5644/",
         "teaser": null
       },{
         "title": "About",

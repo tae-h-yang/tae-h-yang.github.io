c08538fac253c428ff10b622ebe3c8083f08f1d3
tae-h-yang <taehoon.yang98@gmail.com>
2024-01-25 00:45:18 -0500

    Added a spring 2024 course

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -137,6 +137,12 @@ var store = [{
         "url": "/courses/2023-5-8-EECE5644/",
         "teaser": null
       },{
+        "title": "EECE5639 Computer Vision",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/courses/2024-1-9-EECE5639/",
+        "teaser": null
+      },{
         "title": "About",
         "excerpt":"여기엔 Markdown문법으로 적으면 된다. 나는 블로그를 만들었을때 시간이 많이 들었지만, 다른사람들은 이글을 보고 조금 더 빨리 만들었으면 좋겠다.  ","categories": [],
         "tags": ["Blog"],
